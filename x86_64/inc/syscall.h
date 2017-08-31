02e961ce478f8bc66776002c079a97ff95f9cf67
stefanbidi <stefanbidi@gmail.com>
2017-08-31 15:34:10 -0400

    * .gitignore: Ignore object files. * x86_64/inc/syscall.h: Macros for syscalls. * x86_64/inc/sysdep.h: Add boilerplate macros. * x86_64/false.S, * x86_64/true.S: Implement true and false utilities.

---
+++ x86_64/inc/syscall.h
@@ -0,0 +1,97 @@
+/* syscall.h
+
+   Copyright (C) 2017 Stefan Bidigaray
+
+   Written by: Stefan Bidigaray
+   Date: August 2017
+
+   This file is part of Nucleus Utilities.
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef __SYSCALL_H__
+#define __SYSCALL_H__
+
+#include <sys/syscall.h>
+
+.macro _syscall _syscall_num:req
+	movl	\_syscall_num, %eax
+	syscall
+.endm
+
+.macro sys_access
+	_syscall $SYS_access
+.endm
+
+.macro sys_chdir
+	_syscall $SYS_chdir
+.endm
+
+.macro sys_chroot
+	_syscall $SYS_chroot
+.endm
+
+.macro sys_close
+	_syscall $SYS_close
+.endm
+
+.macro sys_creat
+	_syscall $SYS_creat
+.endm
+
+.macro sys_execve
+	_syscall $SYS_execve
+.endm
+
+.macro sys_exit
+	_syscall $SYS_exit
+.endm
+
+.macro sys_getcwd
+	_syscall $SYS_getcwd
+.endm
+
+.macro sys_getpid
+	_syscall $SYS_getpid
+.endm
+
+.macro sys_gettimeofday
+	_syscall $SYS_gettimeofday
+.endm
+
+.macro sys_ioctl
+	_syscall $SYS_ioctl
+.endm
+
+.macro sys_mkdir
+	_syscall $SYS_mkdir
+.endm
+
+.macro sys_open
+	_syscall $SYS_open
+.endm
+
+.macro sys_read
+	_syscall $SYS_read
+.endm
+
+.macro sys_readlink
+	_syscall $SYS_readlink
+.endm
+
+.macro sys_rmdir
+	_syscall $SYS_rmdir
+.endm
+
+.macro sys_rt_sigaction
+	_syscall $SYS_rt_sigaction
+.endm
+
+.macro sys_write
+	_syscall $SYS_write
+.endm
+
+#endif /* __SYSCALL_H__ */
---
+++ x86_64/inc/sysdep.h
@@ -0,0 +1,40 @@
+/* sysdep.h
+
+   Copyright (C) 2017 Stefan Bidigaray
+
+   Written by: Stefan Bidigaray
+   Date: August 2017
+
+   This file is part of Nucleus Utilities.
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef __SYSDEP_H__
+#define __SYSDEP_H__
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define PTR_SIZE 8
+
+#define FUNC_ENTER \
+	pushq	%rbp; \
+	movq	%rsp, %rbp;
+
+#define FUNC_EXIT \
+	movq	%rbp, %rsp; \
+	popq	%rbp;
+
+#define DECL_FUNC(name) \
+	.global name; \
+	.type name, @function; \
+	.align 16;
+
+#endif /* __SYSDEP_H__ */
